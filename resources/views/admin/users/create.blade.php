701e5d818e49d8ec54294a0926d9eda1fd8a4178
= <=>
2020-01-31 20:42:15 +0300

    Users page working without photos

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -1,1 +1,56 @@
-<h1>admin/create</h1>
+@extends('layouts.admin')
+
+@section('content')
+    <div class="panel panel-primary">
+        <div class="panel-heading">
+            <h2 class="panel-title">Add user</h2>
+        </div>
+        <div class="panel-body">
+
+
+
+        {!! Form::open(['method'=>'POST', 'action'=>'AdminUsersController@store', 'files'=>true]) !!}
+           <div class="form-group">
+               {!! Form::label('name', 'Name') !!}
+               {!! Form::text('name', null, ['class'=>'form-control']) !!}
+           </div>
+
+
+            <div class="form-group">
+                {!! Form::label('email', 'Email') !!}
+                {!! Form::email('email', null, ['class'=>'form-control']) !!}
+            </div>
+
+            <div class="form-group">
+                {!! Form::label('role_id', 'Role') !!}
+                {!! Form::select('role_id', [''=>'Choose role'] + $roles, null, ['class'=>'form-control']) !!}
+            </div>
+
+            <div class="form-group">
+                {!! Form::label('is_active', 'Status') !!}
+                {!! Form::select('is_active', array(0 => 'Not Active', 1 => 'Active'), 0, ['class'=>'form-control']) !!}
+            </div>
+
+            <div class="form-group">
+                {!! Form::label('password', 'Password') !!}
+                {!! Form::password('password', ["class" => "form-control"]) !!}
+            </div>
+
+            <div class="form-group">
+                {!! Form::label('file', 'Image') !!}
+                {!! Form::file('file', ['class'=>'form-control']) !!}
+            </div>
+
+
+           <div class="form-group">
+               {!! Form::submit('create user', ['class'=>'btn btn-primary']) !!}
+           </div>
+        {!! Form::close() !!}
+
+            @include('includes.form_errors')
+
+
+
+        </div>
+    </div>
+    @endsection
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,1 +1,49 @@
-<h1>admin/index</h1>
+@extends('layouts.admin')
+
+@section('content')
+
+    <div class="panel panel-primary">
+        <div class="panel-heading">
+            <h2 class="panel-title">Users</h2>
+        </div>
+        <div class="panel-body">
+            <table class="table table-hover table-striped table-bordered">
+                <thead>
+                  <tr>
+                      <th>Id</th>
+                      <th>Name</th>
+                      <th>Email</th>
+                      <th>Role</th>
+                      <th>Status</th>
+                      <th>Created</th>
+                      <th>Updated</th>
+                  </tr>
+                </thead>
+                <tbody>
+                 @if($users)
+                     @foreach($users as $user)
+                         <tr>
+                             <td>{{$user->id}}</td>
+                             <td>{{$user->name}}</td>
+                             <td>{{$user->email}}</td>
+                             <td>{{$user->role->name}}</td>
+                             <td>{{$user->is_active == 1 ? 'Active' : 'Not Active'}}</td>
+                             <td>{{$user->created_at}}</td>
+                             <td>{{$user->updated_at}}</td>
+                         </tr>
+                         @endforeach
+                     @endif
+
+
+                </tbody>
+              </table>
+        </div>
+    </div>
+
+
+
+
+
+
+
+    @endsection
